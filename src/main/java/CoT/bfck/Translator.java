516d1b38cf77bbe4b651833c0b16d43cba301acd
Thomas Greaux <thomas.greaux@etu.unice.fr>
2016-12-19 19:49:33 +0100

    Avancement sur la generation de code: supporte les instructions + - > < completement, debut de [ et ] (methode naive)

--- src/main/java/CoT/bfck/Translator.java
+++ src/main/java/CoT/bfck/Translator.java
@@ -1,6 +1,6 @@
 package CoT.bfck;
 
-import CoT.bfck.Command.Command;
+import CoT.bfck.Command.*;
 import com.sun.org.apache.xpath.internal.SourceTree;
 
 import java.io.File;
@@ -14,15 +14,19 @@ import java.util.ArrayList;
  */
 public class Translator {
 
-    private File file = new File("../../../../files/Output/traduction.txt");
+    private static final String default_path = "D:/\"Polytech Nice\"/Projet/bfck_renew/bfck/files/Output/traduction.txt";
     private PrintWriter pw;
 
-    public Translator(){
-        try{
-            pw = new PrintWriter(file);
-        } catch(FileNotFoundException fnfe) {
-            System.out.println("Output file for translation not found" + fnfe.toString());
-            System.exit(7);
+    public Translator(String path){
+        if(path == null) pw = new PrintWriter(System.out);
+        else{
+            File file = new File(path);
+            try{
+                pw = new PrintWriter(file);
+            } catch(FileNotFoundException fnfe) {
+                System.out.println("Output file for translation not found" + fnfe.toString());
+                System.exit(7);
+            }
         }
     }
 
@@ -33,18 +37,27 @@ public class Translator {
 
     public void translate(ArrayList<Command> commands){
         pw.println("public class Main{");
-        pw.println("\tpublic static void main(String[] args){");
-        pw.println("\t\tint[] memory = new int[30000];");
+        //Declaration of variables
+        pw.println("\tprivate int[] memory;");
+        pw.println("\tprivate int i;");
+        pw.println();
+        //Constructor
+        pw.println("\tpublic Main(){");
+        pw.println("\t\ti = 0;");
+        pw.println("\t\tmemory = new int[30000];");
         pw.println("\t\tfor(int i = 0; i<30000; i++){");
         pw.println("\t\t\tmemory[i] = 0;");
         pw.println("\t\t}");
-        for(Command cmd : commands){
-            for(int i = 0; i < 20; i++){
-
-            }
+        pw.println("\t}");
+        pw.println();
+        //Main program
+        pw.println("\tpublic static void main(String[] args){");
+        for(Command command : commands){
+            pw.println("\t\t" + translate_instruction(command));
         }
         pw.println("\t}");
         pw.println("}");
+        pw.flush();
     }
 
     /**
@@ -57,10 +70,31 @@ public class Translator {
         if(instruct.equals("+")) return "memory[i]++;";
         if(instruct.equals("-")) return "memory[i]--;";
         if(instruct.equals(">")) return "i++;";
-        if(instruct.equals("-")) return "i--;";
+        if(instruct.equals("<")) return "i--;";
+        if(instruct.equals("[")) return "while(memory[i])\t{";
+        if(instruct.equals("]")) return "\t}";
         else{
+            System.out.println("Pas une instruction: " + instruct);
             System.exit(7);
             return "";
         }
     }
+
+    public static void main (String[] args){
+        ArrayList<Command> commands = new ArrayList<Command>();
+        commands.add(new Increment());
+        commands.add(new Increment());
+        commands.add(new Increment());
+        commands.add(new Increment());
+        commands.add(new Decrement());
+        commands.add(new Decrement());
+        commands.add(new Right());
+        commands.add(new Right());
+        commands.add(new Increment());
+        commands.add(new Left());
+        commands.add(new Increment());
+        Translator t = new Translator(null);
+        t.translate(commands);
+    }
+
 }
---
+++ src/main/test/CoT/bfck/TranslatorTest.java
@@ -0,0 +1,8 @@
+package CoT.bfck;
+
+/**
+ * Tests for the Translator class
+ * Created by Thoma on 12/19/2016.
+ */
+public class TranslatorTest {
+}
